89c2d6e607cda98359f712a254c3d4fe1b41759d
regeniaa <regeniaa@student.21-school.ru>
2023-02-28 17:32:48 +0700

    20

--- mainwindow.h
+++ mainwindow.h
@@ -1,18 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "controller_start.h"
-/*
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include "p.h"
-#ifdef __cplusplus
-}
-#endif
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -23,6 +11,8 @@ extern "C" {
 #include <cstdlib>
 #include <iostream>
 #include <string>
+
+#include "controller_start.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -36,7 +26,6 @@ class MainWindow : public QMainWindow {
  public:
   MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
-  // void build_graph (QString new_label);
 
  private:
   Ui::MainWindow *ui;
@@ -52,12 +41,6 @@ class MainWindow : public QMainWindow {
   void dot();
   void build_graph(QString new_label);
   void substitution_x(QString new_label);
-  /*void on_baton_cred_clicked();
-  void on_pushButton_result_clicked();
-  void scan_data();
-  void calculate_deposit_without_cap();
-  void calculate_deposit_with_cap();
-  void clean_deposit_data();*/
   void sum_razn();
   void butten_x();
 
